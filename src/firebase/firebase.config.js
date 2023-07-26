f58627f1ebda3fee573e597ae909484905007f5e
shiamhub <skshiam@gmail.com>
2023-07-26 23:02:34 +0600

    added envcon

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+    apiKey: "AIzaSyBERHdxY4_Wv4NoDsJUNLV3ylHNpUSfFOA",
+    authDomain: "new-job-find.firebaseapp.com",
+    projectId: "new-job-find",
+    storageBucket: "new-job-find.appspot.com",
+    messagingSenderId: "65287735832",
+    appId: "1:65287735832:web:e63756a96f9d406d4f516d"
 };
 
 // Initialize Firebase
